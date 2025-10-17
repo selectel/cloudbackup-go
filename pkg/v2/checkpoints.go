d3354fea0a7487ca1c36ddfac45c26a89ffd2a51
Вениамин Мурашов <45630710+MurashovVen@users.noreply.github.com>
2025-10-17 14:03:28 +0400

    Initialize base client

---
+++ pkg/v2/checkpoints.go
@@ -0,0 +1,99 @@
+package cloudbackup
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+type (
+	Checkpoint struct {
+		ID              string           `json:"id"`
+		PlanID          string           `json:"plan_id"`
+		CreatedAt       string           `json:"created_at"`
+		Status          string           `json:"status"`
+		CheckpointItems []CheckpointItem `json:"checkpoint_items"`
+	}
+
+	CheckpointItem struct {
+		ID              string             `json:"id"`
+		BackupID        string             `json:"backup_id"`
+		ChainID         string             `json:"chain_id"`
+		CheckpointID    string             `json:"checkpoint_id"`
+		CreatedAt       string             `json:"created_at"`
+		BackupCreatedAt string             `json:"backup_created_at"`
+		IsIncremental   bool               `json:"is_incremental"`
+		Status          string             `json:"status"`
+		Resource        CheckpointResource `json:"resource"`
+	}
+
+	CheckpointResource struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+		Type string `json:"type"`
+	}
+
+	CheckpointsQuery struct {
+		PlanName   string
+		VolumeName string
+		Limit      int
+		Marker     string
+	}
+
+	CheckpointsResponse struct {
+		Checkpoints []*Checkpoint `json:"checkpoints"`
+		Total       int           `json:"total"`
+	}
+)
+
+func (q *CheckpointsQuery) queryParamsRaw() string {
+	params := url.Values{}
+	if q == nil {
+		return params.Encode()
+	}
+
+	if q.PlanName != "" {
+		params.Add("plan_name", q.PlanName)
+	}
+
+	if q.VolumeName != "" {
+		params.Add("volume_name", q.VolumeName)
+	}
+
+	if q.Limit != 0 {
+		params.Add("limit", strconv.Itoa(q.Limit))
+	}
+
+	if q.Marker != "" {
+		params.Add("marker", q.Marker)
+	}
+
+	return params.Encode()
+}
+
+func (client *ServiceClient) Checkpoints(ctx context.Context, q *CheckpointsQuery) (*CheckpointsResponse, *ResponseResult, error) {
+	queryParams := ""
+	if qRaw := q.queryParamsRaw(); q != nil && qRaw != "" {
+		queryParams = "?" + qRaw
+	}
+
+	u := fmt.Sprintf("%s/checkpoints/%s", client.Endpoint, queryParams)
+
+	responseResult, err := client.DoRequest(ctx, http.MethodGet, u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	if responseResult.Err != nil {
+		return nil, responseResult, responseResult.Err
+	}
+
+	res := CheckpointsResponse{}
+	err = responseResult.ExtractResult(&res)
+	if err != nil {
+		return nil, responseResult, err
+	}
+
+	return &res, responseResult, nil
+}
